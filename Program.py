36a7b1c76d8409e4b778d9b0bdf818f6909d6043
ChrisH0mmers <chrishamers@outlook.com>
2024-06-22 16:04:05 +0200

    Fixed bug

--- Program.py
+++ Program.py
@@ -29,21 +29,19 @@ class Application(Frame):
         self.submit_bttn = Button(self, text="Submit", command=self.reveal)
         self.submit_bttn.grid(row=3, column=0, columnspan=2, pady=10)
 
-        self.secret_txt = Text(self, width=35, height=5, wrap=WORD)
-        self.secret_txt.grid(row=4, column=0, columnspan=2, sticky=W)
+
 
     def reveal(self):
         username = self.user_ent.get()
         password = self.pw_ent.get()
         if username == "Ho-Wah" and password == "m@ng0":
-            message = "Login complete"
             self.show_menu()
         else:
-            message = "Login failed"
+            self.fail_lbl = Label(self, text ="Login failed")
+            self.fail_lbl.grid(row=4, column =1, sticky =W)
             self.pw_ent.delete(0, END)  
 
-        self.secret_txt.delete(1.0, END)
-        self.secret_txt.insert(1.0, message)
+
 
     def show_menu(self):
         self.inst_lbl.grid_remove()
@@ -52,7 +50,8 @@ class Application(Frame):
         self.pw_lbl.grid_remove()
         self.pw_ent.grid_remove()
         self.submit_bttn.grid_remove()
-        self.secret_txt.grid_remove()
+        self.fail_lbl.grid_remove()
+
 
         bold_font = font.Font(weight="bold")
         menu_lbl = Label(self, text="Choose amount with numbers", font=bold_font)
