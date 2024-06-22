72fc4bcc8f59598e39a0f261ba27da80649245db
ChrisH0mmers <chrishamers@outlook.com>
2024-06-22 11:03:31 +0200

    abc

--- Program.py
+++ Program.py
@@ -6,7 +6,8 @@ from tkinter.ttk import *
 
 class Application(Frame):
     def __init__(self, master):
-        super(Application, self).__init__(master)
+        super().__init__(master)
+        self.master = master
         self.grid()
         self.create_widgets()
     
@@ -23,8 +24,8 @@ class Application(Frame):
         self.pw_ent1 = Entry(self)
         self.pw_ent1.grid(row =1, column = 1, sticky = W)
 
-        self.pw_lbl = Label(self, text = "Password")
-        self.pw_lbl.grid(row =2, column = 0, sticky = W)
+        self.pw_lbl1 = Label(self, text = "Password")
+        self.pw_lbl1.grid(row =2, column = 0, sticky = W)
 
         self.pw_ent = Entry(self, show ='*')
         self.pw_ent.grid(row =2, column = 1, sticky = W)
@@ -37,20 +38,36 @@ class Application(Frame):
         self.secret_txt.grid(row=6, column=0, columnspan =2, sticky =W)
 
     def reveal(self):
+
         User1 = self.pw_ent1.get()
         Password1 = self.pw_ent.get()
         if User1 == "Hoa-Wah" and Password1 == "m@ng0":
             message = "Login complete"
+            self.menu()
         else:
             message= "Login failed"
             self.pw_ent.delete(0, END)  
 
         self.secret_txt.delete(0.0, END)
         self.secret_txt.insert(0.0, message)
+
+    def menu(self):
+
+        bold_font = font.Font(weight="bold")
+
+        self.inst_lbl.grid_remove()
+        self.pw_lbl1.grid_remove()
+        self.pw_ent.grid_remove()
+        self.pw_lbl.grid_remove()
+        self.pw_ent1.grid_remove()
+        self.secret_txt.grid_remove()
+        self.submit_bttn1.grid_remove()
+
+        bold_font = font.Font(weight="bold")
+        menu_lbl = Label(self, text="Choose amount with numbers", font=bold_font)
+        menu_lbl.grid(row=0, column=0, sticky=W)
+
         
-
-
-
 
 
 
@@ -62,9 +79,6 @@ root.geometry("1400x800")
 app = Application(master=root)
 app.pack()
 # endregion Root
-
-
-
 
 root.mainloop()
 
