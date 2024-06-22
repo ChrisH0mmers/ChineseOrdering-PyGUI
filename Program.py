c5f004aa06b37ff0c5b401f5ea468b7b5b4c74a6
ChrisH0mmers <chrishamers@outlook.com>
2024-06-22 10:02:48 +0200

    Added visuality

--- Program.py
+++ Program.py
@@ -1,4 +1,8 @@
 from tkinter import *
+import tkinter.font as font
+
+
+
 
 class Application(Frame):
     def __init__(self, master):
@@ -8,29 +12,39 @@ class Application(Frame):
     
     def create_widgets(self):
 
-        self.inst_lbl = Label(self, text="Log-In Required")
+        bold_font = font.Font(weight="bold")
+
+        self.inst_lbl = Label(self, text="Log-In Required", font=bold_font)
         self.inst_lbl.grid(row=0, column=0, columnspan=2, sticky=W)
 
         self.pw_lbl = Label(self, text = "Username")
         self.pw_lbl.grid(row =1, column = 0, sticky = W)
 
+        self.pw_ent = Entry(self)
+        self.pw_ent.grid(row =1, column = 1, sticky = W)
+
         self.pw_lbl = Label(self, text = "Password")
         self.pw_lbl.grid(row =2, column = 0, sticky = W)
 
-
-        self.inst_bttn1 = Button(self, text="Start System")
-        self.inst_bttn1.grid(row=3, column=0, columnspan=2, pady=10)
-
+        self.pw_ent = Entry(self)
+        self.pw_ent.grid(row =2, column = 1, sticky = W)
 
 
+        self.submit_bttn1 = Button(self, text="Submit", command = self.reveal)
+        self.submit_bttn1.grid(row=3, column=0, columnspan=2, pady=10)
+    def reveal(self):
+        contents= self.pw_ent.get()
 
-
+# region Root
 root = Tk()
 root.title("ChineseOrder Sys")
 root.geometry("1400x800")
 
 app = Application(master=root)
 app.pack()
+# endregion Root
+
+
 
 root.mainloop()
 
