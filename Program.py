e96b022b536b5b7e0e3e6ccecaeb8e068a848dd1
ChrisH0mmers <chrishamers@outlook.com>
2024-06-22 09:37:56 +0200

    Added a label and a button

--- Program.py
+++ Program.py
@@ -1,11 +1,21 @@
 from tkinter import *
+
 root = Tk()
 root.title("ChineseOrder Sys")
 root.geometry("1400x800")
+
+
 app=Frame(root)
+app.pack()
+app.grid
 
 
 
+lbl = Label(app, text = "Chinese System")
+lbl.grid()
+
+bttn1 = Button(app, text = "Start System")
+bttn1.grid()
 
 
 
